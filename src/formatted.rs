418c9ffc35b3775930ad7db77b79e39155af04ec
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-03-21 11:09:18 +0200

    code improvements

--- src/formatted.rs
+++ src/formatted.rs
@@ -1,18 +1,17 @@
 use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 
-use chrono::{NaiveDateTime, NaiveTime, TimeDelta, Timelike};
-
 use super::playlist::Playlist;
 use super::track::Track;
 use super::types::{FileFormat, PlaylistType};
-use super::{formatted, utils};
+use super::utils;
 
 /// Read a formatted CSV playlist file.
 pub fn read_formatted_csv(path: &Path, data: Vec<BTreeMap<String, String>>) -> anyhow::Result<Playlist> {
     // TODO: fix data reading
-    let (playlist_name, playlist_date) = formatted::parse_formatted_playlist_info(&data[0]);
-    let tracks = formatted::parse_formatted_tracks_from_data(&data, playlist_date);
+    let playlist_name = path.file_stem().unwrap().to_string_lossy().to_string();
+    let playlist_date = None;
+    let tracks = parse_formatted_tracks_from_data(&data);
     let total_duration = utils::get_total_playtime(&tracks);
     let max_artist_length: usize = tracks.iter().map(|t| t.artist_length()).max().unwrap_or(0);
     let max_title_length: usize = tracks.iter().map(|t| t.title_length()).max().unwrap_or(0);
@@ -32,79 +31,17 @@ pub fn read_formatted_csv(path: &Path, data: Vec<BTreeMap<String, String>>) -> a
     })
 }
 
-/// Parse first row data from a Serato playlist.
-///
-/// This row should contain the playlist name and start datetime.
-pub fn parse_formatted_playlist_info(data: &BTreeMap<String, String>) -> (String, Option<NaiveDateTime>) {
-    let playlist_name = match data.get("name") {
-        None => String::new(),
-        Some(n) => n.to_string(),
-    };
-    // timestamp, for example "10.01.2019, 20.00.00 EET"
-    let mut playlist_date = match data.get("start time") {
-        None => None,
-        Some(time) => NaiveDateTime::parse_from_str(time, "%d.%m.%Y, %H.%M.%S %Z").ok(),
-    };
-    if playlist_date.is_none() && !playlist_name.is_empty() {
-        playlist_date = utils::extract_datetime_from_name(&playlist_name);
-    }
-    (playlist_name, playlist_date)
-}
-
 /// Parse track data from dictionary
-pub fn parse_formatted_tracks_from_data(
-    data: &[BTreeMap<String, String>],
-    playlist_date: Option<NaiveDateTime>,
-) -> Vec<Track> {
-    let start_date = playlist_date.unwrap_or_default().date();
-    let initial_tracks: Vec<Track> = {
-        data[1..]
-            .iter()
-            .map(|row| {
-                let start_time: Option<NaiveDateTime> = row
-                    .get("start time")
-                    .and_then(|t| NaiveTime::parse_from_str(t, "%H.%M.%S %Z").ok())
-                    .map(|n| NaiveDateTime::new(start_date, n));
-
-                let end_time: Option<NaiveDateTime> = row
-                    .get("end time")
-                    .and_then(|t| NaiveTime::parse_from_str(t, "%H.%M.%S %Z").ok())
-                    .map(|n| NaiveDateTime::new(start_date, n));
-
-                let play_time = match row.get("playtime") {
-                    Some(t) => NaiveTime::parse_from_str(t, "%H:%M:%S").ok().and_then(|n| {
-                        let hours = TimeDelta::try_hours(i64::from(n.hour()))?;
-                        let minutes = TimeDelta::try_minutes(i64::from(n.minute()))?;
-                        let seconds = TimeDelta::try_seconds(i64::from(n.second()))?;
-                        Some(hours + minutes + seconds)
-                    }),
-                    None => start_time.and_then(|start| end_time.map(|end| end - start)),
-                };
-                Track::new_with_time(
-                    row.get("artist").unwrap_or(&"".to_string()).to_string(),
-                    row.get("name").unwrap_or(&"".to_string()).to_string(),
-                    start_time,
-                    end_time,
-                    play_time,
-                )
-            })
-            .collect()
-    };
-
-    // Remove consecutive duplicates
-    let mut index: usize = 0;
-    let mut tracks: Vec<Track> = vec![initial_tracks[0].clone()];
-    for track in initial_tracks[1..].iter() {
-        let previous_track = &tracks[index];
-        if *previous_track == *track {
-            // duplicate track -> add playtime to previous and skip
-            tracks[index] += track.play_time;
-            tracks[index].end_time = track.end_time;
-        } else {
-            // new track, append to playlist
-            tracks.push(track.clone());
-            index += 1;
-        }
+pub fn parse_formatted_tracks_from_data(data: &[BTreeMap<String, String>]) -> Vec<Track> {
+    let mut tracks: Vec<Track> = Vec::new();
+    for row in data.iter() {
+        let artist = row.get("artist").unwrap_or(&"".to_string()).to_string();
+        let name = row.get("name").unwrap_or(&"".to_string()).to_string();
+        // TODO: parse times
+        // let playtime = row.get("playtime");
+        // let start_time = row.get("start time");
+        // let end_time = row.get("end time");
+        tracks.push(Track::new(artist, name))
     }
     tracks
 }
--- src/serato.rs
+++ src/serato.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 
-use chrono::{NaiveDateTime, NaiveTime, TimeDelta, Timelike};
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeDelta, Timelike};
 
 use super::playlist::Playlist;
 use super::track::Track;
@@ -97,56 +97,27 @@ pub fn parse_serato_tracks_from_data(
     playlist_date: Option<NaiveDateTime>,
 ) -> Vec<Track> {
     let start_date = playlist_date.unwrap_or_default().date();
-    let initial_tracks: Vec<Track> = {
-        data[1..]
-            .iter()
-            .map(|row| {
-                let start_time: Option<NaiveDateTime> = row
-                    .get("start time")
-                    .and_then(|t| NaiveTime::parse_from_str(t, "%H.%M.%S %Z").ok())
-                    .map(|n| NaiveDateTime::new(start_date, n));
-
-                let end_time: Option<NaiveDateTime> = row
-                    .get("end time")
-                    .and_then(|t| NaiveTime::parse_from_str(t, "%H.%M.%S %Z").ok())
-                    .map(|n| NaiveDateTime::new(start_date, n));
-
-                let play_time = match row.get("playtime") {
-                    Some(t) => NaiveTime::parse_from_str(t, "%H:%M:%S").ok().and_then(|n| {
-                        let hours = TimeDelta::try_hours(i64::from(n.hour()))?;
-                        let minutes = TimeDelta::try_minutes(i64::from(n.minute()))?;
-                        let seconds = TimeDelta::try_seconds(i64::from(n.second()))?;
-                        Some(hours + minutes + seconds)
-                    }),
-                    None => start_time.and_then(|start| end_time.map(|end| end - start)),
-                };
-                Track::new_with_time(
-                    row.get("artist").unwrap_or(&"".to_string()).to_string(),
-                    row.get("name").unwrap_or(&"".to_string()).to_string(),
-                    start_time,
-                    end_time,
-                    play_time,
-                )
-            })
-            .collect()
-    };
+    let initial_tracks: Vec<Track> = data
+        .iter()
+        .skip(1)
+        .map(|row| parse_track_with_time_from_row(start_date, row))
+        .collect();
 
     // Remove consecutive duplicates
-    let mut index: usize = 0;
-    let mut tracks: Vec<Track> = vec![initial_tracks[0].clone()];
-    for track in initial_tracks[1..].iter() {
-        let previous_track = &tracks[index];
-        if *previous_track == *track {
-            // duplicate track -> add playtime to previous and skip
-            tracks[index] += track.play_time;
-            tracks[index].end_time = track.end_time;
-        } else {
-            // new track, append to playlist
-            tracks.push(track.clone());
-            index += 1;
+    let mut deduped_tracks: Vec<Track> = Vec::new();
+    for track in initial_tracks {
+        if let Some(last_track) = deduped_tracks.last_mut() {
+            if *last_track == track {
+                // Add playtime of duplicate track to previous
+                *last_track += track.play_time;
+                last_track.end_time = track.end_time;
+                continue;
+            }
         }
+        deduped_tracks.push(track);
     }
-    tracks
+
+    deduped_tracks
 }
 
 pub fn read_serato_txt_lines(initial_lines: Vec<Vec<String>>) -> Vec<Vec<String>> {
@@ -170,13 +141,13 @@ pub fn read_serato_txt_lines(initial_lines: Vec<Vec<String>>) -> Vec<Vec<String>
         .collect();
     // Extract each column item from the line, starting from the end of the line
     column_start_indices.reverse();
-    let mut serato_lines: Vec<Vec<String>> = vec![];
+    let mut serato_lines: Vec<Vec<String>> = Vec::new();
     for line in initial_lines {
         // skip the divider lines
         if line[0].chars().all(|c| c == '-') {
             continue;
         }
-        let mut split_line: Vec<String> = vec![];
+        let mut split_line: Vec<String> = Vec::new();
         let mut remaining_line: String = line[0].to_string();
         for index in &column_start_indices {
             // some lines do not contain data in all fields
@@ -196,4 +167,33 @@ pub fn read_serato_txt_lines(initial_lines: Vec<Vec<String>>) -> Vec<Vec<String>
         serato_lines.push(split_line);
     }
     serato_lines
+}
+
+fn parse_track_with_time_from_row(start_date: NaiveDate, row: &BTreeMap<String, String>) -> Track {
+    let start_time: Option<NaiveDateTime> = row
+        .get("start time")
+        .and_then(|t| NaiveTime::parse_from_str(t, "%H.%M.%S %Z").ok())
+        .map(|time| NaiveDateTime::new(start_date, time));
+
+    let end_time: Option<NaiveDateTime> = row
+        .get("end time")
+        .and_then(|t| NaiveTime::parse_from_str(t, "%H.%M.%S %Z").ok())
+        .map(|time| NaiveDateTime::new(start_date, time));
+
+    let play_time = match row.get("playtime") {
+        Some(t) => NaiveTime::parse_from_str(t, "%H:%M:%S").ok().and_then(|n| {
+            let hours = TimeDelta::try_hours(i64::from(n.hour()))?;
+            let minutes = TimeDelta::try_minutes(i64::from(n.minute()))?;
+            let seconds = TimeDelta::try_seconds(i64::from(n.second()))?;
+            Some(hours + minutes + seconds)
+        }),
+        None => start_time.and_then(|start| end_time.map(|end| end - start)),
+    };
+    Track::new_with_time(
+        row.get("artist").unwrap_or(&"".to_string()).to_string(),
+        row.get("name").unwrap_or(&"".to_string()).to_string(),
+        start_time,
+        end_time,
+        play_time,
+    )
 }
